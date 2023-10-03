b0567242b9e7cc7becf318eda15d88f739d83b3f
omkar <omienfield@gmail.com>
2023-10-03 15:05:47 +0530

    initial commit

---
+++ src/main/java/com/one/employeenestpoc/employeenestpoc/EmployeeNestPocApplication.java
@@ -0,0 +1,13 @@
+package com.one.employeenestpoc.employeenestpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeNestPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeNestPocApplication.class, args);
+	}
+
+}
